5231ef485e5857bafc81c0ea5af4a59d50ccc3ae
GioachinoGreco <126901047+GioachinoGreco@users.noreply.github.com>
2023-03-05 22:53:42 -0800

    Add files via upload

--- RPS.py
+++ RPS.py
@@ -58,18 +58,29 @@ paper_cpu_image_rect = paper_image.get_rect()
 scissors_cpu_image = pg.image.load(os.path.join(game_folder, 'scissorshand.png')).convert()
 scissors_cpu_image_rect = scissors_image.get_rect()
 
+def draw_text(text, size, color, x, y):
+    font_name = pg.font.match_font('arial')
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x,y)
+    screen.blit(text_surface, text_rect)
+
 choices0 = ["rock", "paper", "scissors"]
 
 # computer decides what to choose
 def cpu_randchoice():
     # global variable that stores the computer's choice
-    global cpu_choice
-    cpu_choice = choices0[randint(0,2)]
-    return cpu_choice
+    choice = choices0[randint(0,2)]
+    print("computer decides... " + choice)
+    return choice
 
 player_choice = ""
 
 running = True
+
+start_screen = True
+
 
 while running:
     # forces the frame rate to 30 frames per second
@@ -93,53 +104,148 @@ while running:
             # if the mouse_coords are on the pixels of an image, it prints text
             if rock_image_rect.collidepoint(mouse_coords):
                 player_choice = "rock"
+                start_screen = False
+                print("GAME STARTED")
                 cpu_randchoice()
+                cpu_choice = cpu_randchoice()
             elif paper_image_rect.collidepoint(mouse_coords):
                 player_choice = "paper"
+                start_screen = False
+                print("GAME STARTED")
                 cpu_randchoice()
+                cpu_choice = cpu_randchoice()
             elif scissors_image_rect.collidepoint(mouse_coords):
                 player_choice = "scissors"
+                start_screen = False
+                print("GAME STARTED")
                 cpu_randchoice()
+                cpu_choice = cpu_randchoice()
             else:
                 print("not a valid choice")
 
     ########### INPUT ###########
     # HCI - human computer interaction...
 
-    # set positions
-
-    # rock image position
-    rock_image_rect.x = 10
-    # scissors image position
-    scissors_image_rect.y = 400
-    scissors_image_rect.x = 10
-    # paper image position
-    paper_image_rect.x = 520
-    paper_image_rect.y = 250
-
-    # cpu rock image posiiton
-    rock_cpu_image_rect
-
-
-    # update
-
     ###### draw ######
 
     # fills the screen with a color
     screen.fill(BLACK)
-    # blit = draw a picture
-    if player_choice == "":
-        screen.blit(rock_image, rock_image_rect)
-        screen.blit(paper_image, paper_image_rect)
-        screen.blit(scissors_image, scissors_image_rect)
+    
+    # blit = draw picture
+    screen.blit(rock_image, rock_image_rect)
+    screen.blit(paper_image, paper_image_rect)
+    screen.blit(scissors_image, scissors_image_rect)
 
+    # start screen before the game starts
+    if start_screen:
+        draw_text("click one of the options to play rock paper scissors", 22, WHITE, 700, HEIGHT/10)
 
+        # set positions
+        # rock image position
+        rock_image_rect.x = 10
+        rock_image_rect.y = 10
+        # scissors image position
+        scissors_image_rect.y = 400
+        scissors_image_rect.x = 10
+        # paper image position
+        paper_image_rect.x = 520
+        paper_image_rect.y = 400
+
+    
+    # this is what happens when the mouse is clicked
+    if start_screen == False:
+        # cpu images position
+        rock_cpu_image_rect.x = 520
+        rock_cpu_image_rect.y = 10
+
+        paper_cpu_image_rect.x = 520
+        paper_cpu_image_rect.y = 10
+
+        scissors_cpu_image_rect.x = 520
+        scissors_cpu_image_rect.y = 10
+        if player_choice == "":
+            screen.blit(rock_image, rock_image_rect)
+            screen.blit(paper_image, paper_image_rect)
+            screen.blit(scissors_image, scissors_image_rect)
+            
+
+# checks to see who won
     if player_choice == "rock":
-        screen.blit(rock_image, rock_image_rect)
-    if player_choice == "scissors":
-        screen.blit(scissors_image, scissors_image_rect)
+        if cpu_choice == "rock":
+            rock_cpu_image_rect.x = 500
+            screen.blit(rock_image, rock_image_rect)
+            screen.blit(rock_image, rock_cpu_image_rect)
+            print("you tied")
+            sleep(3)
+            break
+            
+        if cpu_choice == "paper":
+            paper_cpu_image_rect.x = 500
+            screen.blit(rock_image, rock_image_rect)
+            screen.blit(paper_image, paper_cpu_image_rect)
+            print("You lost!")
+            sleep(3)
+            break
+            
+        if cpu_choice == "scissors":
+            scissors_cpu_image_rect.x = 500
+            screen.blit(rock_image, rock_image_rect)
+            screen.blit(scissors_image, scissors_cpu_image_rect)
+            print("you won!")
+            sleep(3)
+            break
+
     if player_choice == "paper":
-        screen.blit(paper_image, paper_image_rect)
+        if cpu_choice == "rock":
+            rock_cpu_image_rect.x = 500
+            screen.blit(paper_image, paper_image_rect)
+            screen.blit(rock_image, rock_cpu_image_rect)
+            print("you won!")
+            sleep(3)
+            break
+            
+        if cpu_choice == "paper":
+            paper_cpu_image_rect.x = 500
+            screen.blit(paper_image, paper_image_rect)
+            screen.blit(paper_image, paper_cpu_image_rect)
+            print("You tied")
+            sleep(3)
+            break
+            
+        if cpu_choice == "scissors":
+            scissors_cpu_image_rect.x = 500
+            screen.blit(paper_image, paper_image_rect)
+            screen.blit(scissors_image, scissors_cpu_image_rect)
+            print("you lost!")
+            sleep(3)
+            break
+
+    if player_choice == "scissors":
+        if cpu_choice == "rock":
+            rock_cpu_image_rect.x = 500
+            screen.blit(scissors_image, scissors_image_rect)
+            screen.blit(rock_image, rock_cpu_image_rect)
+            print("you lost")
+            sleep(3)
+            break
+            
+        if cpu_choice == "paper":
+            paper_cpu_image_rect.x = 500
+            screen.blit(scissors_image, scissors_image_rect)
+            screen.blit(paper_image, paper_cpu_image_rect)
+            print("You Won!")
+            sleep(3)
+            break
+            
+        if cpu_choice == "scissors":
+            scissors_cpu_image_rect.x = 500
+            screen.blit(scissors_image, scissors_image_rect)
+            screen.blit(scissors_image, scissors_cpu_image_rect)
+            print("you lost!")
+            sleep(3)
+            break
+
+    
 
 
 
